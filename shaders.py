7ac9e9cd35decbc8c81089ae428245ce83f5bddb
Stargazing-11 <samuelkifle199314@gmail.com>
2022-06-05 13:11:30 +0300

    added fragement_shader

--- shaders.py
+++ shaders.py
@@ -17,3 +17,18 @@ void main()
     v_texture = a_texture;
 }
 """
+
+fragment_src = """
+# version 330
+
+in vec2 v_texture;
+
+out vec4 out_color;
+
+uniform sampler2D s_texture;
+
+void main()
+{
+    out_color = texture(s_texture, v_texture);
+}
+"""
