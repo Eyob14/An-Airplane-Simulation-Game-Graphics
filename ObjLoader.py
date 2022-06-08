864322ea4e3135acbfa7e345cec4faf013d7e8db
Stargazing-11 <samuelkifle199314@gmail.com>
2022-06-08 13:27:20 +0300

    added object loader

---
+++ ObjLoader.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+class ObjLoader:
+    buffer = []
+
+    def search_data(data_values, coordinates, skip, data_type):
+        for d in data_values:
+            if d == skip:
+                continue
+            if data_type == 'float':
+                coordinates.append(float(d))
+            elif data_type == 'int':
+                coordinates.append(int(d)-1)
+
+
+    # sorted vertex buffer for use with glDrawArrays function
+    def create_sorted_vertex_buffer(indices_data, vertices, textures, normals):
+        for i, ind in enumerate(indices_data):
+            if i % 3 == 0: # sort the vertex coordinates
+                start = ind * 3
+                end = start + 3
+                ObjLoader.buffer.extend(vertices[start:end])
+            elif i % 3 == 1: # sort the texture coordinates
+                start = ind * 2
+                end = start + 2
+                ObjLoader.buffer.extend(textures[start:end])
+            elif i % 3 == 2: # sort the normal vectors
+                start = ind * 3
+                end = start + 3
+                ObjLoader.buffer.extend(normals[start:end])
+
+
+   
