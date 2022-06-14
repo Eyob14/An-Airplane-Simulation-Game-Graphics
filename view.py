74584bca96dd247f0215d23c04f5444ea16fe10f
Eyob14 <eyobzebene3580@gmail.com>
2022-06-14 13:34:06 +0300

    new pygame window created

--- view.py
+++ view.py
@@ -23,3 +23,32 @@ def vertex_Attribute(buffer_name, index):
     # plane normals
     glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, buffer_name.itemsize * 8, ctypes.c_void_p(20))
     glEnableVertexAttribArray(2)
+
+def new_window():
+    background_colour = (50,150,225)
+    (width, height) = (500, 500)
+    screen = pygame.display.set_mode((width, height))
+    textfont = pygame.font.SysFont("monospace", 70)
+    textfonttwo = pygame.font.SysFont("monospace", 25)
+
+    pygame.display.set_caption('Game Detail')
+    screen.fill(background_colour)
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        textTBD = textfont.render("GAME OVER!", True, (0, 0, 0))
+        textTwo = textfonttwo.render("press enter to restart the game", True, (0, 0, 0))
+        
+        
+        screen.blit(textTBD, (55, 200))
+        screen.blit(textTwo, (20, 300))
+        
+        keys = pygame.key.get_pressed()  
+        if keys[pygame.K_RETURN]:
+            running = False
+            initial()
+        pygame.display.update()
+VAO, VBO = None, None        
